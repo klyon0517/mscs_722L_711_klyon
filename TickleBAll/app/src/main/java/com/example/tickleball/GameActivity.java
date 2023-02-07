a2335199b0b892d88011e70a88895b3c3c266c78
klyon0517@gmail.com <klyon0517@gmail.com>
2023-02-06 19:42:14 -0500

    Added if/then for success/fail to GameActivity for Project 1.

--- TickleBAll/app/src/main/java/com/example/tickleball/GameActivity.java
+++ TickleBAll/app/src/main/java/com/example/tickleball/GameActivity.java
@@ -1,12 +1,24 @@
 package com.example.tickleball;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    // Pull in game information from DB and assign to variables
+    // btn name
+    // idle vid
+    // success vid
+    // fail vid
+
+    String success_type = "head";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +26,35 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
     }
 
-    // Need to set this up so it can launch either success or fail
-    // based on which button is named as the success button
-    // from the database call
-    public void LaunchFail(View view) {
-        Intent tickleIntent = new Intent(this, FailActivity.class);
-        startActivity(tickleIntent);
+    /*
+     *  The game information is pulled from MongoDB
+     *  Success or Fail scenarios are based on the
+     *  button text (determined during the ButtonPress).
+     *  If the button text equals the string from the database,
+     *  the SuccessActivity Intent is started.
+     *  Otherwise, the FailActivity Intent is chosen
+     */
+
+    public void ButtonPress(View view) {
+
+        Log.d(LOG_TAG, success_type);
+
+        Button b = (Button) view;
+        String btn_txt = b.getText().toString();
+        Log.d(LOG_TAG, btn_txt);
+
+        if (success_type.equals(btn_txt)) {
+
+            Intent tickleIntent = new Intent(this, SuccessActivity.class);
+            startActivity(tickleIntent);
+
+         } else {
+
+            Intent tickleIntent = new Intent(this, FailActivity.class);
+            startActivity(tickleIntent);
+
+         }
+
     }
 
 }
--- TickleBAll/app/src/main/java/com/example/tickleball/MainActivity.java
+++ TickleBAll/app/src/main/java/com/example/tickleball/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.tickleball;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
