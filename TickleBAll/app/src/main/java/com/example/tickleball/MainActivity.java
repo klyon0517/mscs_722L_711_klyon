7bf6d10b92809a7c50634e2b9cd2bf7b86c2592e
klyon0517@gmail.com <klyon0517@gmail.com>
2023-02-14 19:24:00 -0500

    Finally have Volley working with my PHP REST API. Also needed to create a network_security_config and set cleartextTrafficPermitted to true.

--- TickleBAll/app/src/main/java/com/example/tickleball/MainActivity.java
+++ TickleBAll/app/src/main/java/com/example/tickleball/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,24 +13,36 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    // private static final String TAG = MainActivity.class.getName();
-    // private StringRequest stringRequest;
 
-    TextView dbTest;
-    String url = "http://localhost/rest_api/query_test.php";
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbTest = findViewById(R.id.textView);
+        Button btnRequest = (Button) findViewById(R.id.button2);
+
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v){
+
+                  sendAndRequestResponse();
+
+              }
+          }
+
+        );
+
+        /* dbTest = findViewById(R.id.textView);
 
         //RequestQueue initialized
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -53,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 dbTest.setText("bummer!");
 
             }
-        }); /*{
+        }), {
 
             @Nullable
             @Override
@@ -65,10 +77,40 @@ public class MainActivity extends AppCompatActivity {
                 return param;
 
             }
-        }; */
+        };
 
         // RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
-        requestQueue.add(request);
+        requestQueue.add(request); */
+
+    }
+
+    private void sendAndRequestResponse() {
+
+        String url = "http://10.0.2.2/rest_api/query_test.php";
+
+        //RequestQueue initialized
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        //display the response on screen
+        // private StringRequest request;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                //display the response on screen
+                Toast.makeText(getApplicationContext(), "Response: " + response.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.d(TAG, "Error :" + error.toString());
+            }
+        });
+
+        queue.add(request);
 
     }
 
@@ -78,10 +120,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(idleIntent);
     }
 
-    public void LaunchHiScore(View view) {
+    /* public void LaunchHiScore(View view) {
         Log.d(LOG_TAG, "HiScore");
         Intent hiscoreIntent = new Intent(this, HiscoreActivity.class);
         startActivity(hiscoreIntent);
-    }
+    } */
 
 }
